fdd83f3c5fe1c0fc72163ad6a132db3022f49881
Mahmoud Adel <mahmoudadel5556@gmail.com>
2024-02-18 17:56:43 +0200

    Add Colors

---
+++ app/src/main/java/com/example/medisim/data/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.medisim.data
+
+object Constants {
+}
--- app/src/main/java/com/example/medisim/ui/theme/Color.kt
+++ app/src/main/java/com/example/medisim/ui/theme/Color.kt
@@ -1,11 +1,55 @@
 package com.example.medisim.ui.theme
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// background
+val BackgroundDark = Color(0xFF001C30 )
+val BackgroundLight = Color(0xFFD6E8EE  )
+
+// text
+val TextDark = Color(0xFFD6E8EE)
+val TextLight = Color(0xFF001B48)
+val TextHint = Color(0xFF8D7F7F)
+
+// components
+val ComponentDark = Color(0xFF073C61)
+val ComponentLight = Color(0xFF97CADB)
+val CommonComponent = Color(0xFF02457A)
+
+// Buttons
+
+
+@Composable
+fun animatedShimmerColor(shimmerColors:List<Color>): Brush {
+
+    val transition = rememberInfiniteTransition(label = "")
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
+    return Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+
+}
