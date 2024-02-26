405159c9fdff2cc9502b489327080f00834f42c9
Mahmoud Adel <mahmoudadel5556@gmail.com>
2024-02-26 23:14:44 +0200

    add calculator screen
    * and some design in medical test screen

--- app/src/main/java/com/example/medisim/presentation/components/ImageComponents.kt
+++ app/src/main/java/com/example/medisim/presentation/components/ImageComponents.kt
@@ -3,19 +3,31 @@ package com.example.medisim.presentation.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.medisim.ui.theme.HelperColor1
@@ -63,7 +75,8 @@ fun ImageButtonClick(
     paddingValue:Int ,
     imageWidth: Int = 60,
     imageHeight: Int = 60,
-    onButtonClick:() -> Unit ) {
+    onButtonClick:() -> Unit
+) {
     Image(
         painter = painterResource(id = image),
         modifier = modifier
@@ -74,5 +87,70 @@ fun ImageButtonClick(
         ,
         contentDescription = "image button",
     )
+
+}
+
+@Composable
+fun ImageButtonWithText(
+    image:Int,
+    modifier: Modifier = Modifier,
+    text:String,
+    textFont: Int = 30,
+    height: Int = 150,
+    textFontWight: FontWeight = FontWeight.Bold,
+    maxLines: Int = 1,
+    onButtonClick:() -> Unit
+) {
+
+
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background)
+            .height(height.dp)
+            .padding(top = 10.dp)
+            .clickable {
+                // here when user click on Advice
+                onButtonClick()
+            }
+    ){
+        Image(
+            painter = painterResource(id = image),
+            modifier = Modifier
+                .height(height.dp)
+                .background(MaterialTheme.colorScheme.background)
+                .clip(RoundedCornerShape(16.dp))
+            ,
+            contentDescription = "",
+            contentScale = ContentScale.Crop
+        )
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(height.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Black,
+                        Color.Transparent,
+                        Color.Black,
+                    )
+                )
+            ),
+            contentAlignment = Alignment.Center,
+
+        ){
+            Text(
+                text = text,
+                modifier = modifier,
+                fontSize = textFont.sp,
+                fontWeight = textFontWight,
+                color = Color.White,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = maxLines,
+            )
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/medisim/presentation/homeScreens/bottomNavigationScreens/MedicalTestScreen.kt
+++ app/src/main/java/com/example/medisim/presentation/homeScreens/bottomNavigationScreens/MedicalTestScreen.kt
@@ -1,8 +1,41 @@
 package com.example.medisim.presentation.homeScreens.bottomNavigationScreens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.medisim.R
+import com.example.medisim.presentation.components.ButtonClickOn
+import com.example.medisim.presentation.components.TextLabel
 
 @Composable
 fun MedicalTestScreen() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ){
+        TextLabel(
+            text = stringResource(R.string.medical_tests),
+            textFont = 22,
+            textFontWight = FontWeight.Bold
+        )
+//        LottieAnimationShow(
+//            animationResId = ,
+//            size = ,
+//            padding = ,
+//            paddingBottom =
+//        )
+        Spacer(modifier = Modifier.weight(1f))
+        ButtonClickOn(
+            buttonText = stringResource(R.string.upload_test),
+            modifier = Modifier.padding(bottom = 25.dp),
+            paddingValue = 0
+        ) {
 
+        }
+
+    }
 }
